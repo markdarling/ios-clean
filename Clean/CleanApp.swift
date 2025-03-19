672a42ba0a9d82673aa5a1faea639bb47d6f0775
Mark Darling <mark@markd.net>
2025-03-19 19:32:10 +0000

    Initial Commit

---
+++ Clean/CleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanApp.swift
+//  Clean
+//
+//  Created by Mark Darling on 19/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
